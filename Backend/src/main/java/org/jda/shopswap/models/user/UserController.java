137a092a25392b73f65715a4d7f33c81f2228233
UJKMjuandi <jgutierrezcc@unsa.edu.pe>
2024-06-19 23:06:52 -0500

    Se creo el archivo UserService y se movieron los metodos de UserController a UserService para una mejor legibilidad en el codigo

--- Backend/src/main/java/org/jda/shopswap/models/user/UserController.java
+++ Backend/src/main/java/org/jda/shopswap/models/user/UserController.java
@@ -16,20 +16,11 @@ public class UserController {
     private UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
     private final JwtService jwtService;
+    private final UserService userService;
 
     @PostMapping("/saveUser")
     public User saveUser(@RequestBody User userData, @RequestHeader String authorization) {
-        Long adminId = jwtService.getUserIdFromToken(jwtService.getTokenFromHeader(authorization));
-        User user = User.builder()
-                .username(userData.getUsername())
-                .password(passwordEncoder.encode(userData.getPassword()))
-                .email(userData.getEmail())
-                .address(userData.getAddress())
-                .userCreatedId(adminId)
-                .role(userData.getRole())
-                .status(true)
-                .build();
-        return userRepository.save(user);
+        return userService.createUser(userData, authorization);
     }
 
     @GetMapping("/all")
@@ -39,27 +30,12 @@ public class UserController {
 
     @PutMapping("/update/{id}")
     public User updateUser(@PathVariable int id, @RequestBody User userData, @RequestHeader String authorization) {
-        Long adminId = jwtService.getUserIdFromToken(jwtService.getTokenFromHeader(authorization));
-        User user = userRepository.findById(id).orElseThrow();
-        user.setUsername(userData.getUsername());
-        if (!userData.getPassword().equals(user.getPassword())) {
-            user.setPassword(passwordEncoder.encode(userData.getPassword()));
-        }
-        user.setEmail(userData.getEmail());
-        user.setUserModifiedId(adminId);
-        user.setAddress(userData.getAddress());
-        user.setRole(userData.getRole());
-        user.setStatus(userData.isEnabled());
-        return userRepository.save(user);
+        return userService.updateUser(id, userData, authorization);
     }
 
     @PutMapping("/deactivate/{id}")
     public User deactivateUser(@PathVariable int id, @RequestHeader String authorization) {
-        Long adminId = jwtService.getUserIdFromToken(jwtService.getTokenFromHeader(authorization));
-        User user = userRepository.findById(id).orElseThrow();
-        user.setUserModifiedId(adminId);
-        user.status = false;
-        return userRepository.save(user);
+        return userService.deactivateUser(id, authorization);
     }
 
     @DeleteMapping("/delete/{id}")
