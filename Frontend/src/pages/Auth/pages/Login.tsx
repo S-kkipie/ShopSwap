a94aba6b76dbb87fbd37cfb2381620a3c24ef560
S-kkipie <admamaniq@unsa.edu.pe>
2024-06-22 19:03:37 -0500

    solucionando lo de que no se guardaba la info del usuario

--- Frontend/src/pages/Auth/pages/Login.tsx
+++ Frontend/src/pages/Auth/pages/Login.tsx
@@ -9,7 +9,7 @@ function Login() {
 
     return !isAuth ? (
         <div className="flex">
-            <div className="flex flex-col  w-5/12 rounded-r-3xl  h-screen bg-primary">
+            <div className="hidden lg:flex flex-col w-5/12  h-screen bg-primary">
                 <Link className="block w-full" to="/"><h1
                     className="text-3xl font-bold text-secondary m-8">ShopSwap</h1></Link>
                 <div className="flex-1 flex items-center justify-center">
@@ -17,8 +17,8 @@ function Login() {
                 </div>
             </div>
             <div className="flex-1 h-screen flex  items-center justify-center">
-                <div className=" w-1/3">
-                    <h1 className="text-3xl font-bold mb-8 text-center text-primary">Log In</h1>
+                <div className="p-16 lg:p-0 w-full lg:w-1/3">
+                    <h1 className="text-3xl font-bold mb-8 text-center text-primary">Log in</h1>
                     <LoginForm/>
                     <p className="mt-8 ">
                         <span>Don't have an account?</span> <Link className="text-primary font-bold"
